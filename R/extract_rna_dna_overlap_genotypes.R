99bf0b0949436fbbc430b602a6e5e120973fa747
chase mateusiak <chase.mateusiak@gmail.com>
2022-12-17 06:36:57 -0600

    adding visit info

--- R/extract_rna_dna_overlap_genotypes.R
+++ R/extract_rna_dna_overlap_genotypes.R
@@ -143,6 +143,11 @@ parser <- add_option(parser, c('--rna_sample'), type="character",
                     help="sample id used to extract data from the rna gds files",
                     metavar ="'sample.id'")
 
+parser <- add_option(parser, c('--rna_visit'), type="character",
+                    default="1",
+                    help="visit number of rna sample",
+                    metavar ="'1'")
+
 parser <- add_option(parser, c('--dna_sample'), type="character",
                     default="",
                     help="sample id used to extract data from the dna gds files",
@@ -269,6 +274,7 @@ sample_similarity_metric = tibble(
   chr = opt$chr,
   dna_sample = opt$dna_sample,
   rna_sample = opt$rna_sample,
+  rna_visit = opt$visit,
   dna_var_n = init_dna_var_n,
   rna_var_n = init_rna_var_n,
   overlap_var_n = overlap_var_n,
@@ -281,7 +287,7 @@ sample_similarity_metric = tibble(
 
 output_dir  = paste('rna',opt$rna_sample,'dna',opt$dna_sample,sep="_")
 dir.create(output_dir,showWarnings=FALSE)
-file_name = paste0('chr',opt$chr)
+file_name = paste0('rna_visit_', opt$visit, '_', 'chr',opt$chr)
 
 # write full comparison df if -f is set
 if(opt$write_full_comparison){
